19c4219186abadd11d1ca16fb3b3ef645dc6e6af
Andre King <andreking1031@gmail.com>
2020-08-27 18:25:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,10 +42,7 @@ class Song
     @@all.sort_by{|x| x.name}
   end
   def self.new_from_filename(filename)
-    song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
-    song
+    
   end
   
 
